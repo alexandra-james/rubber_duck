ecf5b8f8d84baa9896581768538040573734850a
Alexandra James <alexandrajames@outlook.de>
2023-05-29 17:37:36 +0200

    model correction user and duck

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ApplicationRecord
-  LANGUAGES = ["Ruby", "JavaScript", "Python", "Go", "Java", "Rust",
-               "C", "C++", "C#", "PHP", "TypeScript", "Swift", "Dart"]
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :ducks, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
-  validates :language, presence: true, inclusion: { in: LANGUAGES }
-  validates :price, presence: true, numericality: { only_integer: true }
-  validates :image_url, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
 end
