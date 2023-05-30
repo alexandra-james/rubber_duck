6eb867687df1a3d65ed89d3180a7e89fc594e726
Alexandra James <alexandrajames@outlook.de>
2023-05-30 15:43:51 +0200

    added simple form in new view and create duck method

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,5 +1,5 @@
 class DucksController < ApplicationController
-  before_action :set_duck, only: [:show, :new, :create]
+  before_action :set_duck, only: [:show]
 
   def index
     @ducks = Duck.all
@@ -11,16 +11,29 @@ class DucksController < ApplicationController
   end
 
   def new
+    @duck = Duck.new
     authorize @duck
   end
 
   def create
+    @duck = Duck.new(duck_params)
+    @user = current_user
+    @duck.user = @user
     authorize @duck
+    if @duck.save
+      redirect_to duck_path(@duck)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_duck
     @duck = Duck.find(params[:id])
+  end
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :language, :image_url, :price)
   end
 end
--- app/models/duck.rb
+++ app/models/duck.rb
@@ -6,7 +6,7 @@ class Duck < ApplicationRecord
   has_many :bookings
 
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
   validates :language, presence: true, inclusion: { in: LANGUAGES }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :image_url, presence: true
