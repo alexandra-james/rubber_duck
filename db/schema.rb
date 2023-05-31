936d00d27e7ef0c4a929deae074200a619497796
Md Shoriful Islam <shuvokhan00009@gmail.com>
2023-05-31 11:58:42 +0200

    added geo_coding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_100727) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_090607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_100727) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_ducks_on_user_id"
   end
 
