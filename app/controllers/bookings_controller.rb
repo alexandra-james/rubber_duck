68de7b30f876c6b2d81414e6900b013fc3ae3001
Alexandra James <alexandrajames@outlook.de>
2023-06-01 12:10:24 +0200

    booking new method and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,13 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
     @bookings = policy_scope(Booking)
   end
+
+  def new
+    @duck = Duck.find(params[:duck_id])
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :ducks
-  resources :bookings, only: [:index, :show]
+  resources :ducks do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
 
   # Defines the root path route ("/")
   # root "pages#index"
