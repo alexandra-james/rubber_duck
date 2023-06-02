fc76a1049ed044b59df088ba93bfa7e042a10ecd
Elija Marx <elija.marx@gmail.com>
2023-06-02 15:04:10 +0200

    searchbar done

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -10,4 +10,15 @@ class Duck < ApplicationRecord
   validates :language, presence: true, inclusion: { in: LANGUAGES }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :image_url, presence: true
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_langurage,
+    against: [ :name, :language ],
+    associated_against: {
+      user: [:location]
+    },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
