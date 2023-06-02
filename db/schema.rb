befb91cac82a63ffc7fb9f8a7e22ec51d78bed08
Alexandra James <alexandrajames@outlook.de>
2023-06-02 11:05:27 +0200

    migration for comment attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_090607) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_090442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_090607) do
     t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "comment"
     t.index ["duck_id"], name: "index_bookings_on_duck_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
